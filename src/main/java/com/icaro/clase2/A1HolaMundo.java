551eaec707691744a8ad03265dffebb2d3d1d9f1
JulianAguilar <juli_011292@hotmail.com>
2023-08-31 18:08:48 -0600

    Se agrego todo

---
+++ src/main/java/com/icaro/clase2/A1HolaMundo.java
@@ -0,0 +1,11 @@
+package com.icaro.clase2;
+
+public class A1HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo :)");
+
+        System.out.println("curso");
+    }
+
+
+}
---
+++ src/main/java/com/icaro/clase2/A2ImprimirSemana.java
@@ -0,0 +1,18 @@
+package com.icaro.clase2;
+
+public class A2ImprimirSemana {
+
+    public static void main(String[] args) {
+        System.out.println("Dia 1 - Lunes ");
+        System.out.println("Dia 2 - Martes ");
+        System.out.println("Dia 3 - Miercoles ");
+        System.out.println("Dia 4 - Jueves ");
+        System.out.println("Dia 5 - Viernes ");
+        System.out.println("Dia 6 - Sabado ");
+        System.out.println("Dia 7 - Domingo ");
+
+
+        //concatencion //
+        System.out.println("Hola " + "Mundo " + "Cool");
+    }
+}
